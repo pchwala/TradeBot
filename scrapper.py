ffc6e04aa70b79ff298a0dd29d805b3bd822ea0c
pchwala <przemyslaw.chwala6@gmail.com>
2025-03-06 18:49:55 +0100

    Finished scrapper program

--- scrapper.py
+++ scrapper.py
@@ -26,6 +26,7 @@ class Scrapper:
         self.con_sql = sql.connect("candlestick.db")
         self.cursor_sql = self.con_sql.cursor()
 
+        # Create database if if doesn't exist
         self.cursor_sql.execute("""
         CREATE TABLE IF NOT EXISTS candlestick_data (
             n INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -41,74 +42,69 @@ class Scrapper:
     async def main_loop(self):
         """Main loop running all tasks using asyncio await"""
 
-        #task1 = asyncio.create_task(self.main_api.get_keep_alive_s())
-        #task2 = asyncio.create_task(self.main_api.close_all_stream_connections())
-        #task3 = asyncio.create_task(self.main_api.ping_interval(1))  # ping server with 1s interval to avoid closing connection
-        task4 = asyncio.create_task(self.get_all_symbols())
-        #task5 = asyncio.create_task(self.get_candles_from_time("EURUSD", 1, self.default_time))
+        # Create and await task downloading all specified symbols candlestick data with resolution of 1minute
+        task1 = asyncio.create_task(self.get_all_symbols())
+        result = await task1
 
-        #result = await task1
-        #result = await task2
-        #result = await task3
-        result = await task4
-
-        print("Logout")
+        # Logout and end program
         result = await self.main_api.get_server_time()
         print(result)
+        print("Logout")
         self.main_api.logout()
 
 
     async def get_all_symbols(self):
-        all_symbols = await self.main_api.get_all_symbols()
+        """
+        Downloads specified symbols candlestick data with resolution of 1minute
+        For now it downloads all and only symbols that allow short-selling
+        """
+        all_symbols = await self.main_api.get_all_symbols() # Get all availalble symbols from XTB
         for symbol in all_symbols:
             if symbol['shortSelling'] is True:
-                self.symbols_short.append(symbol['symbol'])
+                self.symbols_short.append(symbol['symbol']) # List of symbols with short-selling enabled
 
-        n = 0
-        keys_to_remove = ["ctmString", "high", "low"]
-        for symbol in self.symbols_short:
-            if n < 20000:   # Temporary
-                print(f"{n},  {symbol}")
-                n += 1
+        keys_to_remove = ["ctmString", "high", "low"] # Only saving ctm, open, close
+        for n, symbol in enumerate(self.symbols_short):
+            print(f"{n},  {symbol}")
+            
+            # Find last saved candle, the most recent one
+            self.cursor_sql.execute("SELECT MAX(ctm) FROM candlestick_data WHERE ticker = ?", (symbol,))
+            time = self.cursor_sql.fetchone() # Result is a tuple e.g. (value,)
+            time = time[0]                    # So take only the value
+            try:
+                # Sending commands faster than 200ms for 6 executing times will cause connection drop, so put the sleep.
+                # It can be done faster by measuring time of executing this function and calling this fucntion 5 times with no delay
+                # and then only calling it after 200ms, rinse repeat, but as this program will be run only once a month
+                # to retrieve a new portion of data and on a 24/7h running server it is not necesarry
+                await asyncio.sleep(0.2)
+                # If there is saved data for current symbol then save only new data
+                # Else save all available data for that symbol
+                if time is not None:
+                    result = await self.get_candles_from_time(symbol, 1, time+60000) # Time plus 1minute to not double already saved value
+                else:
+                    result = await self.get_candles_from_time(symbol, 1, self.default_time)
+            except Exception as e:
+                print(e)
+                continue
                 
-                self.cursor_sql.execute("SELECT MAX(ctm) FROM candlestick_data WHERE ticker = ?", (symbol,))
-                time = self.cursor_sql.fetchone() # Result is a tuple e.g. (value,)
-                try:
-                    await asyncio.sleep(0.2) # Sending commands faster than 200ms will cause connection drop
-                    if time[0] is not None:
-                        result = await self.get_candles_from_time(symbol, 1, time[0]+60000) # Time plus 1minute to not double value
-                    else:
-                        result = await self.get_candles_from_time(symbol, 1, self.default_time)
-                except Exception as e:
-                    print(e)
-                    continue
-                    
-                candles = result["returnData"]["rateInfos"]
-                for candle in candles:
-                    for key in keys_to_remove:
-                        del candle[key]
+            candles = result["returnData"]["rateInfos"]
+            for candle in candles:
+                for key in keys_to_remove:
+                    del candle[key] # Remove non-important and redundant values
 
-                self.cursor_sql.executemany(
-                    f"INSERT INTO candlestick_data (ticker, ctm, open, close, vol) VALUES ('{symbol}', :ctm, :open, :close, :vol)",
-                    candles
-                )
+            # Add new symbol data to the database
+            self.cursor_sql.executemany(
+                f"INSERT INTO candlestick_data (ticker, ctm, open, close, vol) VALUES ('{symbol}', :ctm, :open, :close, :vol)",
+                candles
+            )
 
-                self.con_sql.commit()
-
-        #value = 1739198700000
-        #result = self.search_candles("DEA.US_4", value, value+180000)
-        if result:
-            print("Search Results:")
-            for row in result:
-                print(row)
-        else:
-            print("No matching records found.")
-
+            self.con_sql.commit()
 
         self.con_sql.close()
 
     
     async def get_candles_from_time(self, symbol, period, start):
+        """Retrieve from API all candles for given symbol from start time with specified period"""
         command = {
             "command": "getChartLastRequest",
             "arguments": {
@@ -122,17 +118,6 @@ class Scrapper:
 
         result = await self.main_api.execute_command(command)
         return result
-
-
-    def search_candles(self, ticker, start_ctm, end_ctm):
-        self.cursor_sql.execute("""
-        SELECT * FROM candlestick_data 
-        WHERE ticker = ? AND ctm BETWEEN ? AND ?
-        ORDER BY ctm ASC
-        """, (ticker, start_ctm, end_ctm))
-        return self.cursor_sql.fetchall()
-
-
 
 async def main():
     scrapper = Scrapper('credentials.json')
